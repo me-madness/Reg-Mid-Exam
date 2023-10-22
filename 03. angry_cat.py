4f56b73bd40addbd52e5b10fa5077ff466301f38
me_madness <p_rangel@abv.bg>
2023-10-22 13:57:27 +0200

    03 angry cat it's bullshit

--- 03. angry_cat.py
+++ 03. angry_cat.py
@@ -1,21 +1,43 @@
 # Third task from MidExam
 
 price_rating = input().split(", ")
-entry_point = int(input())
+entry_point = input()
 type_of_items = input()
-# sum_of_price_ratings = 0
-# left_site = 0
-# right_site = 0 
-
+left_site = []
+right_site = []
 for price in price_rating:
-    print(price)
+    if price <= entry_point:
+        left_site.append(price)
+        print(left_site)
+    else:
+        right_site.append(price)    
 
-# if left_site > right_site:
-#     print(f"Left - {sum_of_price_ratings}")
-# elif left_site == right_site:
-#     print(f"Left - {sum_of_price_ratings}")    
+print(left_site)
+print(right_site)        
+# sum_of_price_ratings = 0
+# left_site_cheap_sum = 0
+# left_site_expensive_sum = 0
+# right_site_cheap_sum = 0 
+# right_site_expensive_sum = 0 
+
+# for price in price_rating:
+#     if type_of_items == "cheap":
+#         if price < entry_point:
+#             left_site_cheap_sum += int(price)    
+#         else:
+#             right_site_cheap_sum += int(price)
+#     elif type_of_items == "expensive":
+#         if price >= entry_point:
+#             left_site_expensive_sum += int(price)
+#         else:
+#             right_site_expensive_sum += int(price)
+            
+            
+                        
+# if left_site_cheap_sum >= right_site_cheap_sum:
+#     print(f"Left - {left_site_cheap_sum}")   
 # else:    
-#     print(f"Right - {sum_of_price_ratings}")
+#     print(f"Right - {right_site_cheap_sum}")
 
 # 1, 5, 1
 # 1
