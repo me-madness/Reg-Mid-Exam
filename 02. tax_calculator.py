af1c40dc68659acd5942748503901e34e4108dcb
me_madness <p_rangel@abv.bg>
2023-10-22 12:58:50 +0200

    02 it's done

--- 02. tax_calculator.py
+++ 02. tax_calculator.py
@@ -5,19 +5,25 @@ total_tax_collected = 0
 
 for vehicle in vehicles: 
    total_tax_to_pay = 0
-   car_type = vehicle[0]
-   years = int(vehicle[1])
-   miles = int(vehicle[2])
+   new_vehicle = vehicle.split()
+   car_type = new_vehicle[0]
+   years = int(new_vehicle[1])
+   miles = int(new_vehicle[2])
    if car_type == "family":
       total_tax_to_pay = (50 - 5 * years) + ((miles // 3000) * 12)
+      total_tax_collected +=total_tax_to_pay
+      print(f"A {car_type} car will pay {total_tax_to_pay:.2f} euros in taxes.")    
    elif car_type == "heavyDuty":
-      total_tax_to_pay = (80 - 8 * years) + ((miles // 3000) * 14)
+      total_tax_to_pay = (80 - 8 * years) + ((miles // 9000) * 14)
+      total_tax_collected +=total_tax_to_pay
+      print(f"A {car_type} car will pay {total_tax_to_pay:.2f} euros in taxes.")      
    elif car_type == "sports":
-      total_tax_to_pay = (100 - 9 * years) + ((miles // 3000) * 18)
+      total_tax_to_pay = (100 - 9 * years) + ((miles // 2000) * 18)
+      total_tax_collected +=total_tax_to_pay
+      print(f"A {car_type} car will pay {total_tax_to_pay:.2f} euros in taxes.") 
    else:
       print("Invalid car type.")
-   print(f"A {car_type} car will pay {total_tax_to_pay:.2f} euros in taxes.") 
-   total_tax_collected +=total_tax_to_pay
+      
     
 print(f"The National Revenue Agency will collect {total_tax_collected:.2f} euros in taxes.")    
 # family 3 7210>>van 4 2345>>heavyDuty 9 31000>>sports 4 7410
--- 03. angry_cat.py
+++ 03. angry_cat.py
@@ -1,2 +1,22 @@
 # Third task from MidExam
 
+price_rating = input().split(", ")
+entry_point = int(input())
+type_of_items = input()
+# sum_of_price_ratings = 0
+# left_site = 0
+# right_site = 0 
+
+for price in price_rating:
+    print(price)
+
+# if left_site > right_site:
+#     print(f"Left - {sum_of_price_ratings}")
+# elif left_site == right_site:
+#     print(f"Left - {sum_of_price_ratings}")    
+# else:    
+#     print(f"Right - {sum_of_price_ratings}")
+
+# 1, 5, 1
+# 1
+# cheap
